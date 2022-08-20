6c8c883a4d8d0937501e2f4b8188ed18c8a18858
Ash Simmonds <ashsimmonds@gmail.com>
2022-08-20 12:39:05 +0930

    Update InspectionCard.tsx

--- components/InspectionCard.tsx
+++ components/InspectionCard.tsx
@@ -23,14 +23,37 @@ export function inspectionMagick(
 }
 
 
+export function RandomPic() {
+
+    const superRandomThing = Math.floor(Math.random() * 10)
+    const imgSrc = 'https://source.unsplash.com/random/400x150?house,' + superRandomThing
+
+    return (
+        <>
+            <img src={imgSrc} alt="junk food" />
+        </>
+    )
+}
+
 
 export default function InspectionCard(inspection: Inspection) {
     return (
-        <div key={inspection.id} className='my-4' >
-            <h2><Link href={`/inspection/${inspection.id}`}>{'Inspection ID: ' + inspection.id || 'new'}</Link></h2>
-            <h3><Link href={`/house/${inspection.house_id}`}>{'House ID: ' + inspection.house_id || 'new'}</Link></h3>
-            <p>Notes: {inspection.notes}</p>
-            <p>Inspection date: {inspection.inspection_date}</p>
+        <div key={inspection.id} className='my-8 card w-96 bg-base-200 shadow-xl' >
+            <figure>
+                <RandomPic />
+            </figure>
+            <div className="card-body">
+
+                <h2><Link href={`/inspection/${inspection.id}`}>{'Inspection ID: ' + inspection.id || 'new'}</Link></h2>
+                <h3><Link href={`/house/${inspection.house_id}`}>{'House ID: ' + inspection.house_id || 'new'}</Link></h3>
+                <p>Notes: {inspection.notes}</p>
+                <p>Inspection date: {inspection.inspection_date}</p>
+                <div className="card-actions justify-end">
+                    <Link href={`/inspection/${inspection.id}`}>
+                        <button className="btn btn-primary">View</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
